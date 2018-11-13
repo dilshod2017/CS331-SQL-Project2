2d728efe203d9463a22c971759c78bf76f82bd7a
dilshod2017 <dilik2005@yahoo.com>
2018-11-13 12:14:37 -0400

    Team table with turncate, remove and add constraint, loadManager table

--- teamTable.sql
+++ teamTable.sql
@@ -27,51 +27,186 @@ create table Process.WorkflowSteps(
 			,GroupName varchar(30) NULL DEFAULT ('group #2') -- for the group
 		  );
 
-		select * from sys.tables
-		select * into dbo.tempTable from [CH01-01-Fact].Data
-		select * into [CH01-01-Fact].Data from dbo.tempTable
 
 		--/////////////////////////////////////////////////////////////
-		truncate table [CH01-01-Fact].Data; --first turnicate
-		alter table [CH01-01-Fact].Data add classTime varchar(5) default '0900-1045',
-		                                    LastName varchar(30) DEFAULT ('Dilshod'),
-											FirstName varchar(30) DEFAULT ('Khodjayev'),
-											DateAdded datetime2 default sysdatetime();
+		--truncate table [CH01-01-Fact].Data; --first turnicate
+		--alter table [CH01-01-Fact].Data add classTime varchar(5) default '0900-1045',
+		--                                    LastName varchar(30) DEFAULT ('Dilshod'),
+		--									FirstName varchar(30) DEFAULT ('Khodjayev'),
+		--									DateAdded datetime2 default sysdatetime();
 			
 		--/////////////////////////////////////////////////////////////
-		exec [Project1].[DropForeignKeysFromStarSchemaData]
-		exec [Project1].[TruncateStarSchemaData]
+		--exec [Project1].[DropForeignKeysFromStarSchemaData]
+		--exec [Project1].[TruncateStarSchemaData]
 
 
-insert into [CH01-01-Fact].Data
-SELECT
-  SalesManagerKey,
-  OccupationKey,
-  TerritoryKey,
-  ProductKey,
-  CustomerKey,
-  ProductCategory,
-  SalesManager,
-  ProductSubcategory,
-  ProductCode,
-  ProductName,
-  Color,
-  ModelName,
-  OrderQuantity,
-  UnitPrice,
-  ProductStandardCost,
-  SalesAmount,
-  OrderDate,
-  MonthName,
-  MonthNumber,
-  Year,
-  CustomerName,
-  MaritalStatus,
-  Gender,
-  Education,
-  Occupation,
-  TerritoryRegion,
-  TerritoryCountry,
-  TerritoryGroup
-FROM tempTable
 
+
+GO
+/****** Object:  StoredProcedure [Project1].[DropForeignKeysFromStarSchemaData]    Script Date: 11/12/2018 7:36:04 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+if OBJECT_ID('[Project1].[DropForeignKeysFromStarSchemaData]','p') is not null
+drop proc [Project1].[DropForeignKeysFromStarSchemaData];
+go
+create PROCEDURE [Project1].[DropForeignKeysFromStarSchemaData]
+AS
+BEGIN
+    SET NOCOUNT ON;
+		/* FK_Data_SalesManagers
+		FK_Data_DimOccupation
+		FK_Data_DimTerritory
+		FK_Data_DimProduct
+		FK_Data_DimCustomer
+		FK_Data_DimOrderDate
+		FK_Data_DimMaritalStatus
+		FK_Data_DimGender
+		*/
+		--this is test
+	alter table [CH01-01-Fact].Data drop constraint if exists FK_Data_DimOccupation;
+ 	alter table [CH01-01-Fact].Data drop constraint if exists FK_Data_DimTerritory;
+	alter table [CH01-01-Fact].Data drop constraint if exists FK_Data_DimProduct;
+	alter table [CH01-01-Fact].Data drop constraint if exists FK_Data_DimCustomer;
+	alter table [CH01-01-Fact].Data drop constraint if exists FK_Data_DimOrderDate;
+	alter table [CH01-01-Fact].Data drop constraint if exists FK_Data_DimMaritalStatus;
+	alter table [CH01-01-Fact].Data drop constraint if exists FK_Data_DimGender;
+	alter table [CH01-01-Dimension].[DimProductSubcategory] drop constraint if exists [FK_DimProductSubcategory_DimProductCategory];
+	alter table [CH01-01-Dimension].[DimProduct] drop constraint if exists [FK_DimProduct_DimProductSubcategory];
+
+
+
+	--truncate table [CH01-01-Fact].Data; --first turnicate
+	--truncate table [CH01-01-Dimension].[DimOccupation]
+	--truncate table [CH01-01-Dimension].[DimOrderDate]
+	--truncate table [CH01-01-Dimension].[DimProduct]
+	--truncate table [CH01-01-Dimension].[DimProductCategory]
+	--truncate table [CH01-01-Dimension].[DimProductSubcategory]
+	--truncate table [CH01-01-Dimension].[DimTerritory]
+	--truncate table [CH01-01-Dimension].[SalesManagers]
+	--truncate table [CH01-01-Dimension].[DimCustomer];
+	--truncate table [CH01-01-Dimension].[DimGender];
+	--truncate table [CH01-01-Dimension].[DimMaritalStatus]
+
+END;
+
+
+
+GO
+/****** Object:  StoredProcedure [Project1].[Load_SalesManagers]    Script Date: 11/12/2018 8:32:57 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		YourName
+-- Create date: 
+-- Description:	
+-- =============================================
+
+if OBJECT_ID('[Project1].[Load_SalesManagers] ','p') is not null
+	drop proc [Project1].[Load_SalesManagers] ;
+go
+
+create PROCEDURE [Project1].[Load_SalesManagers] 
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+
+	INSERT INTO [CH01-01-Dimension].SalesManagers
+	(
+		SalesManagerKey,
+		Category,
+		SalesManager,
+		Office
+	)
+	SELECT DISTINCT
+		SalesManagerKey,
+		old.ProductCategory,
+		SalesManager,
+		Office = CASE
+					 WHEN old.SalesManager LIKE 'Marco%' THEN
+						 'Redmond'
+					 WHEN old.SalesManager LIKE 'Alberto%' THEN
+						 'Seattle'
+					 WHEN old.SalesManager LIKE 'Maurizio%' THEN
+						 'Redmond'
+					 ELSE
+						 'Seattle'
+				 END
+	FROM FileUpload.OriginallyLoadedData AS old
+	ORDER BY old.SalesManagerKey;
+END
+
+go
+
+if OBJECT_ID('[Project1].[AddForeignKeysToStarSchemaData]','p') is not null
+	drop proc [Project1].[AddForeignKeysToStarSchemaData];
+go
+
+GO
+/****** Object:  StoredProcedure [Project1].[AddForeignKeysToStarSchemaData]    Script Date: 11/12/2018 9:34:58 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+create PROCEDURE [Project1].[AddForeignKeysToStarSchemaData]
+
+AS
+BEGIN
+    -- SET NOCOUNT ON added to prevent extra result sets from
+    -- interfering with SELECT statements.
+	alter table [CH01-01-Fact].Data add constraint FK_Data_DimOccupation foreign key([OccupationKey]) references [CH01-01-Dimension].[DimOccupation]([OccupationKey]);
+	alter table [CH01-01-Fact].Data add constraint FK_Data_DimTerritory foreign key([TerritoryKey]) references  [CH01-01-Dimension].[DimTerritory]([TerritoryKey]);
+	alter table [CH01-01-Fact].Data add constraint  FK_Data_DimProduct foreign key([ProductKey]) references [CH01-01-Dimension].[DimProduct]([ProductKey]);
+	alter table [CH01-01-Fact].Data add constraint  FK_Data_DimCustomer foreign key([CustomerKey]) references [CH01-01-Dimension].[DimCustomer]([CustomerKey]);
+	alter table [CH01-01-Fact].Data add constraint FK_Data_DimOrderDate foreign key([OrderDate]) references [CH01-01-Dimension].[DimOrderDate]([OrderDate]);
+	alter table [CH01-01-Fact].Data add constraint FK_Data_DimMaritalStatus foreign key([MaritalStatus]) references [CH01-01-Dimension].[DimMaritalStatus]([MaritalStatus]);
+	alter table [CH01-01-Fact].Data add constraint FK_Data_DimGender foreign key([Gender]) references [CH01-01-Dimension].[DimGender]([Gender]);
+	
+	alter table[CH01-01-Dimension].[DimProductSubcategory] add constraint  [FK_DimProductSubcategory_DimProductCategory] foreign key([ProductCategoryKey]) references [CH01-01-Dimension].[DimProductCategory]([ProductCategoryKey]);
+	--alter table [CH01-01-Fact].Data add constraint [FK_DimProduct_DimProductSubcategory] foreign key([ProductSubcategory]) references [CH01-01-Dimension].[DimProductCategory]([ProductCategoryKey]);
+
+	
+ 	--alter table [CH01-01-Fact].Data add constraint foreign key() references ()
+END;
+
+
+GO
+/****** Object:  StoredProcedure [Project1].[TruncateStarSchemaData]    Script Date: 11/13/2018 12:07:13 PM ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+if OBJECT_ID('[Project1].[TruncateStarSchemaData]','p') is not null
+	drop proc [Project1].[TruncateStarSchemaData];
+go
+
+
+create PROCEDURE [Project1].[TruncateStarSchemaData]
+AS
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+	--something in this table
+	truncate table [CH01-01-Fact].Data; --first turnicate
+	truncate table [CH01-01-Dimension].[DimOccupation];
+	truncate table [CH01-01-Dimension].[DimOrderDate];
+	truncate table [CH01-01-Dimension].[DimProduct];
+	truncate table [CH01-01-Dimension].[DimProductCategory];
+	truncate table [CH01-01-Dimension].[DimProductSubcategory];
+	truncate table [CH01-01-Dimension].[DimTerritory];
+	truncate table [CH01-01-Dimension].[SalesManagers];
+	truncate table [CH01-01-Dimension].[DimCustomer];
+	truncate table [CH01-01-Dimension].[DimGender];
+	truncate table [CH01-01-Dimension].[DimMaritalStatus];
+
+
+
+end
